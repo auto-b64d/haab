a8ab280fab5a9c94ebe07bb4c25bbbf6a8465c94
auto-b64d <dmdjdj0213@gmail.com>
2024-09-25 03:15:30 +0900

    성능 개선 & 버그 수정
    루트 요소에 subtree: true로 감시보단 각 요소에 subtree: false로 각각 감시가 더 효율적

--- src/content.ts
+++ src/content.ts
@@ -1,46 +1,80 @@
 const observe = (
 	target: Node,
-	options: MutationObserverInit,
-	callback: (mutations: MutationRecord[]) => void,
-	once = false,
+	callback: (mutations: MutationRecord[], observer: MutationObserver) => void,
 ) => {
-	new MutationObserver((muts, observer) => {
-		callback(muts)
-		if (once) observer.disconnect()
-	}).observe(target, { childList: true, subtree: true, ...options })
+	new MutationObserver(callback).observe(target, { childList: true })
 }
 
-// unsafe = 코드를 작성한 시점엔 의도한 대로 동작하나 미래에 부작용이 생길 수 있음
-
-observe(document.documentElement, {}, muts => {
-	for (const mut of muts) {
-		for (const n of mut.addedNodes) {
-			const elem = n as Element
-			if (
-				elem instanceof HTMLScriptElement && (
-					(elem.async && !elem.src.includes('hitomi.la')) // unsafe / 현재 async 속성이 적용된 script는 광고 관련밖에 없음
-						|| /\bcreateElement\((['"])script\1\)/.test(elem.innerText)
-				)
-			) {
-				// 광고 스크립트
-				console.log(['DEBUG'], 'removing', elem)
-				elem.remove()
-			} else if (elem instanceof HTMLIFrameElement) {
-				console.log(['DEBUG'], 'removing', elem)
-				elem.sandbox.value = '' // 스크립트 실행 비활성화
-				elem.remove()
-			} else if (
-				elem instanceof HTMLDivElement && !elem.hasAttributes() // unsafe / 현재 아무 속성이 없는 div는 광고 컨테이너밖에 없음
-			) {
-				// 광고 컨테이너 
-				console.log(['DEBUG'], 'removing', elem)
-				elem.remove()
-			}
+// 편의성을 위해 Node 대신 HTMLElement를 사용
+const forEachAdded = (fn: (node: HTMLElement, observer: MutationObserver) => void) =>
+	(muts: MutationRecord[], observer: MutationObserver) => {
+		for (const mut of muts) {
+			for (const node of mut.addedNodes) fn(node as HTMLElement, observer)
 		}
 	}
+
+const work = forEachAdded(e => {
+	// unsafe = 코드를 작성한 시점엔 의도한 대로 동작하나 미래에 부작용이 생길 수 있음
+	
+	let shouldRemove = false
+	if (e instanceof HTMLScriptElement) {
+		if (e.async && !e.src.includes('hitomi.la')) {
+			// unsafe / async 속성이 적용된 script는 gg.js(다운로드 관련) 제외 광고 관련밖에 없음
+			shouldRemove = true
+		} else if (!e.hasAttribute('src') && !document.head.contains(e)) {
+			// unsafe / body 내에 있는 인라인 스크립트는 광고 관련밖에 없음
+			shouldRemove = true
+		}
+	} else if (e instanceof HTMLIFrameElement) {
+		e.sandbox.value = '' // 스크립트 실행 비활성화
+		shouldRemove = true
+	} else if (e instanceof HTMLDivElement && !e.hasAttributes()) {
+		// unsafe / 현재 아무 속성이 없는 div는 광고 컨테이너밖에 없음
+		shouldRemove = true
+	}
+	
+	if (!shouldRemove) return
+	
+	console.log(['DEBUG'], 'removing', e, '|', 'parent:', e.parentElement)
+	e.remove()
 })
 
-onmessage = null
-for (const evt of ['load', 'DOMContentLoaded']) {
-	window.addEventListener(evt, () => onmessage = null)
+const observeUntilFound = (
+	target: Node,
+	filter: (elem: HTMLElement) => boolean,
+	callback: (elem: HTMLElement) => void,
+) => {
+	observe(target, forEachAdded((n, o) => {
+		if (filter(n)) {
+			o.disconnect()
+			callback(n)
+		}
+	}))
 }
+const hasClass = (class_: string) =>
+	(elem: Element) =>
+		elem.classList.contains(class_)
+
+// head, body도 초기에는 존재하지 않을 수 있음
+observeUntilFound(
+	document.documentElement,
+	elem => elem instanceof HTMLHeadElement,
+	head => observe(head, work)
+)
+observeUntilFound(
+	document.documentElement,
+	// 화면에 보이지 않는(visibility: hidden), 곧 교체될 body가 일시적으로 존재할 수 있음 (???)
+	// 그 body가 아니라 최종적으로 쓰이는 body를 얻기 위해 부모 존재 여부로 구분
+	elem => elem instanceof HTMLBodyElement && elem.parentElement != null,
+	body => {
+		observe(body, work)
+		observeUntilFound(
+			body,
+			hasClass('container'),
+			container => {
+				observe(container, work)
+				observeUntilFound(container, hasClass('top-content'), elem => observe(elem, work))
+			},
+		)
+	}
+)
